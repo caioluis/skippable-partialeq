435a0406974e61d2e67fba271b53a6a5d1120c25
Caio Gomes <caiocodes@proton.me>
2024-05-06 19:24:31 +0100

    feat: write basic implementation and tests
    Signed-off-by: Caio Gomes <caiocodes@proton.me>

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, DeriveInput};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[proc_macro_derive(TimelessPartialEq)]
+pub fn partial_eq_except_timestamps(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
+        ..
+    }) = ast.data
+    {
+        named
+    } else {
+        panic!("TimelessPartialEq can only be derived for structs with named fields");
+    };
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let field_comparisons = fields.iter().filter_map(|field| {
+        let ident = &field.ident;
+        let field_type = &field.ty;
+        if ident.as_ref().unwrap().to_string().ends_with("_at") {
+            if field_type
+                .to_token_stream()
+                .to_string()
+                .starts_with("Option")
+            {
+                return Some(quote! { self.#ident.is_none() && other.#ident.is_none() || self.#ident.is_some() && other.#ident.is_some()});
+            }
+            
+            None
+        } else {
+            Some(quote! { &self.#ident == &other.#ident })
+        }
+    });
+
+    let expanded = quote! {
+        impl PartialEq for #name {
+            fn eq(&self, other: &Self) -> bool {
+                #(#field_comparisons)&&*
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
 }
---
+++ tests/derive_macro_test.rs
@@ -0,0 +1,90 @@
+use chrono::{DateTime, TimeZone, Utc};
+
+#[macro_use]
+extern crate timeless_partialeq;
+
+#[test]
+fn it_ignores_fields_ending_with_at() {
+    #[derive(Debug, TimelessPartialEq)]
+    pub struct Post {
+        pub id: i64,
+        pub content: String,
+        pub author: i32,
+        pub created_at: DateTime<Utc>,
+        pub updated_at: Option<DateTime<Utc>>,
+    }
+
+    assert_eq!(
+        Post {
+            id: 1,
+            content: "test".to_string(),
+            author: 1,
+            created_at: Utc.timestamp_millis_opt(1715017040672).unwrap(),
+            updated_at: Some(Utc.timestamp_millis_opt(1715017020672).unwrap()),
+        },
+        Post {
+            id: 1,
+            content: "test".to_string(),
+            author: 1,
+            created_at: Utc::now(),
+            updated_at: Some(Utc::now()),
+        }
+    )
+}
+
+#[test]
+fn it_fails_without_custom_derive_macro() {
+    #[derive(Debug, PartialEq)]
+    pub struct Post {
+        pub id: i64,
+        pub content: String,
+        pub author: i32,
+        pub created_at: DateTime<Utc>,
+        pub updated_at: Option<DateTime<Utc>>,
+    }
+    assert_ne!(
+        Post {
+            id: 1,
+            content: "test".to_string(),
+            author: 1,
+            created_at: Utc.timestamp_millis_opt(1715017040672).unwrap(),
+            updated_at: Some(Utc.timestamp_millis_opt(1715017020672).unwrap()),
+        },
+        Post {
+            id: 1,
+            content: "test".to_string(),
+            author: 1,
+            created_at: Utc::now(),
+            updated_at: Some(Utc::now()),
+        }
+    )
+}
+
+#[test]
+fn it_checks_optional_timestamps_accordingly() {
+    #[derive(Debug, TimelessPartialEq)]
+    pub struct Post {
+        pub id: i64,
+        pub content: String,
+        pub author: i32,
+        pub created_at: DateTime<Utc>,
+        pub updated_at: Option<DateTime<Utc>>,
+    }
+
+    assert_ne!(
+        Post {
+            id: 1,
+            content: "test".to_string(),
+            author: 1,
+            created_at: Utc.timestamp_millis_opt(1715017040672).unwrap(),
+            updated_at: Some(Utc.timestamp_millis_opt(1715017020672).unwrap()),
+        },
+        Post {
+            id: 1,
+            content: "test".to_string(),
+            author: 1,
+            created_at: Utc::now(),
+            updated_at: None,
+        }
+    )
+}
