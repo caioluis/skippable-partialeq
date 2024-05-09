d27fcf2857088f5b2fe98937e30d0bd7126f8ade
Caio Gomes <caiocodes@proton.me>
2024-05-09 12:51:18 +0100

    feat: container attributes for flexible
    Signed-off-by: Caio Gomes <caiocodes@proton.me>

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,27 @@ use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(TimelessPartialEq)]
+
+#[proc_macro_derive(TimelessPartialEq, attributes(exclude_suffix))]
 pub fn partial_eq_except_timestamps(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
+
+    let mut args: Vec<String> = Vec::new();
+    
+    for attr in ast.attrs.iter() {
+        if attr.path().is_ident("exclude_suffix") {
+            let meta_list = attr.meta.require_list();
+            if let Ok(meta_list) = meta_list {
+                for arg in meta_list.tokens.to_token_stream() {
+                    let arg = arg.to_string();
+                    if arg != "," {
+                        args.push(arg);
+                    }
+                } 
+            }
+        }
+    }
+
     let name = &ast.ident;
     let fields = if let syn::Data::Struct(syn::DataStruct {
         fields: syn::Fields::Named(syn::FieldsNamed { named, .. }),
@@ -20,16 +38,19 @@ pub fn partial_eq_except_timestamps(input: TokenStream) -> TokenStream {
     let field_comparisons = fields.iter().filter_map(|field| {
         let ident = &field.ident;
         let field_type = &field.ty;
-        if ident.as_ref().unwrap().to_string().ends_with("_at") {
+        let field = ident.as_ref().unwrap().to_string();
+        if args.is_empty() {
+            args.push("at".to_string());
+        }
+        if args.iter().any(|arg| field.ends_with(&format!("_{}", arg))) {
             if field_type
-                .to_token_stream()
-                .to_string()
-                .starts_with("Option")
+            .to_token_stream()
+            .to_string()
+            .starts_with("Option")
             {
                 return Some(quote! { self.#ident.is_none() && other.#ident.is_none() || self.#ident.is_some() && other.#ident.is_some()});
-            }
-            
-            None
+            }   
+                None
         } else {
             Some(quote! { &self.#ident == &other.#ident })
         }
